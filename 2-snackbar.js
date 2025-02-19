c20ea34a0d11de24ed09a22f3e805d94a7035f8b
naumenkota <104317537+naumenkota@users.noreply.github.com>
2025-02-19 14:59:22 +0000

    Deploying to gh-pages from @ naumenkota/goit-js-hw-10@4f32f0231283f6d52f10c262a0bdc024e20df6c3 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i}from"./assets/vendor-A92OCY9B.js";const l=document.querySelector(".form");l.addEventListener("submit",t=>{t.preventDefault();const s=t.target.elements.delay.value,m=t.target.elements.state.value,r=new Promise((e,o)=>{setTimeout(()=>{m==="fulfilled"?e(s):o(s)},Number(s))});r.then(e=>{i.success({title:"Success",message:`✅ Fulfilled promise in ${e}ms`})}),r.catch(e=>{i.error({title:"Error",message:`❌ Rejected promise in ${e}ms`})})});
+//# sourceMappingURL=2-snackbar.js.map
